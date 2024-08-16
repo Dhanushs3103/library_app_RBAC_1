0b4bd8ee1e42213ecb7501cab135ef7ba14d1293
Dhanushs3103 <dhanushvictor2003@gmail.com>
2024-08-16 22:55:27 +0530

    Created model for registring a user and for creating the books

---
+++ models/books.model.js
@@ -0,0 +1,39 @@
+// packages
+let mongoose = require("mongoose");
+
+//create schema for storing user details
+let bookSchema = mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    author:{
+        type:String,
+        required:true
+    },
+    genre:{
+        type:String,
+        required:true
+    },
+    createdOn:{
+        type:Date,
+        default:Date.now,
+        required:true
+    },
+    pages:{
+        type:Number,
+        required:true
+    },
+    authorId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Users"
+    }
+},{
+    versionKey:false
+})
+
+// creating UserModel out userSchema
+let BookModel = mongoose.model("book",bookSchema)
+
+// exporting the UserModel
+module.exports = BookModel;
---
+++ models/users.model.js
@@ -0,0 +1,43 @@
+// packages
+let mongoose = require("mongoose");
+
+//local imports
+
+// user schema for registering the user.
+let userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true 
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    roles: { 
+        type: [String], 
+        enum: ["creator", "viewer", "viewAll"],
+        default: ["viewAll"], 
+        required: true
+    },
+    booksCreated: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Books"
+    }]
+},{
+    versionKey:false
+});
+
+// UserModel
+let UserModel = mongoose.model("User", userSchema);
+
+// exporting the UserModel
+module.exports = UserModel;
